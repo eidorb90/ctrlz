b221e98ed6f1bdd7468c82585ff48314927f6dc2
eidorb90 <brodieman500@gmail.com>
2025-07-29 15:55:35 -0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ import sys
 import os
 
 import zlib
+import hashlib
 
 
 def main():
@@ -27,9 +28,30 @@ def main():
             _, obj_content = decompressed.split(b'\x00', 1)
             print(obj_content.decode('utf-8'), end="")
 
+    elif command == "hash-object":
+        file_path = sys.argv[3]
+        if file_path:
+            hash = hash_object(file_path)
+            print(hash)
 
     else:
         raise RuntimeError(f"Unknown command #{command}")
+
+def hash_object(file_path: str) -> str:
+    with open(file_path, 'rb') as f:
+        content = f.read()
+
+    hash = hashlib.sha1(content).hexdigest()
+    object_dir = f".git/objects/{hash[:2]}"
+    if not os.path.exists(object_dir):
+        os.makedirs(object_dir)
+    object_path = f"{object_dir}/{hash[2:]}"
+    if not os.path.exists(object_path):
+        with open(object_path, 'wb') as obj_file:
+            obj_file.write(zlib.compress(f"blob {len(content)}\x00".encode() + content))
+            obj_file.close()
+    print(f"Created object {hash} for file {file_path}")
+    return hash
 
 
 if __name__ == "__main__":
